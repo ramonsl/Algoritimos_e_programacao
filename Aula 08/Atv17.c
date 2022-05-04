e0bb264f3777ebd8760e8c01b10e6544e86e5828
Ramon Lummertz <ramonsl@gmail.com>
2022-05-04 16:50:57 -0300

    Update Atv17.c

--- Aula 08/Atv17.c
+++ Aula 08/Atv17.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include <math.h>
-#include <windows.h>
 
 int main(){
-	UINT CPAGE_UTF8 = 65001;
-	UINT CPAGE_DEFAULT = GetConsoleOutputCP();
-	SetConsoleOutputCP(CPAGE_UTF8);
 	float Senha;
 	printf("Insira a senha: ");
 	scanf("%f%*c", &Senha);
-	while (Senha != 4531){
-		printf("Acesso negado, tente novamente...\n");
-		printf("Insira a senha: ");
-		scanf("%f%*c", &Senha);
-	}
+
 	if (Senha == 4531){
 		printf("Acesso liberado...");
+	}else{
+		printf("Acesso Negado...");
+
 	}
 
 	return 0;
